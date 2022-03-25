7d8ade220eb91a53426d4da4b1b84e66f9f43d3d
Olin Kirkland <me@olinkirk.land>
2022-03-25 13:32:19 +0100

    Add quiz builder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+import QuizBuilder from './controllers/QuizBuilder';
+
 function App() {
+  useState(() => {
+    console.log(new QuizBuilder());
+  }, []);
+
   return (
     <>
       <p>Hello World</p>
