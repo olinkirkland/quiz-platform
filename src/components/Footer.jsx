999fdcaf23bcdb90b18d0ca3effa9e2308aefea9
root <root@DESKTOP-GVQ0Q5H.localdomain>
2022-03-25 15:54:46 +0100

    style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,18 @@
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+
 export default function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <p>Copyright Team 6</p>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
