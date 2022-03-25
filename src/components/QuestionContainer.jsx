137fe635e81a1888e672def6c269e1e4dedfdb78
root <root@DESKTOP-GVQ0Q5H.localdomain>
2022-03-25 12:13:16 +0100

    routing

---
+++ src/components/QuestionContainer.jsx
@@ -0,0 +1,10 @@
+import { Question } from "./Question";
+import { useNavigate, useLocation } from "react-router-dom";
+
+export const QuestionContainer = () => {
+  return (
+    <div>
+      <Question />
+    </div>
+  );
+};
